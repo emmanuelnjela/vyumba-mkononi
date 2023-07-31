ed1a407adc3523d88a36feb08b8185ae5e974101
Emmanuel Njela <emmanuelictpro@gmail.com>
2023-07-31 12:05:44 +0300

    Change the design, instead of showing searchbar i show current added houses

--- frontend/src/components/overview/Overview.jsx
+++ frontend/src/components/overview/Overview.jsx
@@ -2,17 +2,35 @@ import OverviewBody from "./overview-body/overviewBody";
 import OverviewHeader from "./overview-header/overviewHeader";
 import OverviewFooter from "./overview-footer/overviewFooter";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../common/navbar/Navbar";
 
 
 function Overview() {
+  const navItems = [
+    {
+      id: 1,
+      path: "/register",
+      link: <button className='btn btn--primary'>JIUNGE</button>,
+    },
+    {
+      id: 2,
+      path: "/login",
+      link: <button className='btn btn--primary'>INGIA</button>,
+    },
+  ];
+
+      
   return (
     <div className="overview">
-      <Routes>
+      <Navbar navBrandHideSize="md" navItems={navItems} />
+      <OverviewBody />
+      <OverviewFooter />
+      {/* <Routes>
         <Route index element={<OverviewHeader />}></Route>
         <Route path="/main" element={<OverviewBody />}>
           <Route index element={<OverviewFooter />} />
         </Route>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
