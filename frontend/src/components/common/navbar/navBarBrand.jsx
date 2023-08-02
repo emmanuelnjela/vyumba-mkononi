e49cef00927192365fecb922855b87a69420985f
Emmanuel Njela <emmanuelictpro@gmail.com>
2023-08-02 16:17:57 +0300

    Solved the issue of navigation in navbar, differnctiated for login or not

--- frontend/src/components/common/navbar/navBarBrand.jsx
+++ frontend/src/components/common/navbar/navBarBrand.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../../imgs/logo.png";
-import Cookies from "universal-cookie";
+import CurrentUserContext from "../../../context/currentUserContext";
+import { useContext } from "react";
+
+
 
 function NavBarBrand({ navItems }) {
   const classes = `navbar__brand-name d--${
     navItems.length > 2 ? "lg" : "md"
   }-block`;
 
-  const cookies = new Cookies()
-  const accessKey = cookies.get("accessKey")
-  
+  const {isLogin} = useContext(CurrentUserContext) 
+
   return (
-    <NavLink to={accessKey ? "/home" : "/"} className="navbar__brand">
+    <NavLink to={isLogin ? "/home" : "/"} className="navbar__brand">
       <img src={logo} alt="" className="navbar__brand-logo" />
       <h6 className={classes}>Vyumba vya Kupanga online</h6>
     </NavLink>
