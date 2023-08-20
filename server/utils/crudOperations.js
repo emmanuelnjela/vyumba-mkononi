04f25f87ec6ba8fd4ec9f07d9f2677a264be487d
Emmanuel Njela <emmanuelictpro@gmail.com>
2023-08-20 21:54:02 +0300

    The crud operation, solved some issues including data handling

--- server/utils/crudOperations.js
+++ server/utils/crudOperations.js
@@ -2,9 +2,17 @@
 export class Crud {
   constructor(dataModel) {
     this._dataModel = dataModel;
-    this._datas = this.getAllDatas();
+    this._datas = [];
+    this.initializeData();
   }
 
+  async initializeData() {
+    try {
+      this._datas = await this.getAllDatas();
+    } catch (error) {
+      throw error;
+    }
+  }
   /**
    * get all data in the DB
    * @returns array of datas
@@ -14,78 +22,87 @@ export class Crud {
       const datas = await this._dataModel.find({});
       return datas;
     } catch (error) {
-        return error
+      throw error;
     }
   }
-
-  /**
+ /**
    * Add new data
    * @param {Object} data
    * @returns datas with updated data or message
    */
   async addData(data) {
-    const dataInDB = await this._dataModel(data)
-    dataInDB.save()
-    return dataInDB
+    try {
+      const dataInDB = new this._dataModel(data);
+      await dataInDB.save();
+      this._datas.push(dataInDB);
+      return dataInDB;
+    } catch (error) {
+      throw error;
+    }
   }
-
   /**
    * Check if data is in DB if not its will return the error message otherwise an object
    * of with data and index of the found data
    * @param {integer} id
-   * @returns object of found data
+   * @returns object of found dataS
    */
-  foundDataInDB(id) {
-    const data = this._datas.find(({ _id }) => _id === parseInt(id));
-    if (!data) return "data not found!";
-    // const index = this._datas.findIndex(({ _id }) => _id === data._id);
-    return { data, id };
+  async foundDataInDB(id) {
+    try {
+      const data = this._datas.find(({ _id }) => _id === parseInt(id));
+      if (!data) throw new Error("Data not found!");
+      return { data, id };
+    } catch (error) {
+      throw error;
+    }
   }
-
   /**
    * Delete the data in the database
    * @param {string} id
    * @returns datas
    */
-  deleteData(id) {
-    const foundData = this.foundDataInDB(id);
-    if (typeof foundData === "string") return foundData;
-
-    const { data: dataInDB } = foundData;
-    this._datas = this._datas.filter(({ _id }) => dataInDB._id !== _id);
-    //   console.log(this._datas)
-    return this._datas;
+  async deleteData(id) {
+    try {
+      const foundData = await this.foundDataInDB(id);
+      const { data: dataInDB } = foundData;
+      this._datas = this._datas.filter(({ _id }) => _id !== dataInDB._id);
+      return this._datas;
+    } catch (error) {
+      throw error;
+    }
   }
-
   /**
    * Update the data in the database
    * @param {integer} id
    * @param {string} datas
    * @returns updated data in array
    */
-  updateData(id, dataElements) {
-    const foundData = this.foundDataInDB(id);
-    if (typeof foundData === "string") return foundData;
+  async updateData(id, dataElements) {
+    try {
+      const foundData = await this.foundDataInDB(id);
+      const { data: dataInDB } = foundData;
 
-    const { data: dataInDB, index: dataInDBIndex } = foundData;
-    for (var elem of dataElements) {
-      const { name, value } = elem;
-      dataInDB[name] = value;
+      for (const elem of dataElements) {
+        const { name, value } = elem;
+        dataInDB[name] = value;
+      }
+
+      await dataInDB.save(); // Save the updated data back to the database
+      return this._datas;
+    } catch (error) {
+      throw error;
     }
-
-    this._datas[dataInDBIndex] = dataInDB;
-    return this._datas;
   }
-
   /**
    * View an individual data in the database
    * @param {string} id
    * @returns found data
    */
-  getData(id) {
-    const foundData = this.foundDataInDB(id);
-    if (typeof foundData === "string") return foundData;
-
-    return foundData.data;
+  async getData(id) {
+    try {
+      const foundData = await this.foundDataInDB(id);
+      return foundData.data;
+    } catch (error) {
+      throw error;
+    }
   }
 }
