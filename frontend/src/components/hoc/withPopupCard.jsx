1b970344c043788ad3fd51112428332a9d80ca15
Emmanuel Njela <emmanuelictpro@gmail.com>
2023-09-21 11:34:28 +0300

    Instead of hardcode the navlink to attribute to /home now the withpopupcard can go to any location as it can defined at linkto as a prop

--- frontend/src/components/hoc/withPopupCard.jsx
+++ frontend/src/components/hoc/withPopupCard.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function withPopUpCard(Content) {
   const WithPopUpCard = (props) => (
     <div className={`popup-card`}>
-      <NavLink to={"/home"} className="cancel rounded rounded--primary">
+      <NavLink to={props.linkTo} className="cancel rounded rounded--primary">
           <i className="fas fa-x "></i>
         </NavLink>
       <Content {...props} />
